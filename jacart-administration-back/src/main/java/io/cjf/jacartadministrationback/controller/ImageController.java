e2ff81f877b1a7495fc2e3dec0b86d9e0ecea18f
WCY <13216142707>
2020-03-12 21:17:38 +0800

    图片上传  后端

--- jacart-administration-back/src/main/java/io/cjf/jacartadministrationback/controller/ImageController.java
+++ jacart-administration-back/src/main/java/io/cjf/jacartadministrationback/controller/ImageController.java
@@ -2,6 +2,7 @@ package io.cjf.jacartadministrationback.controller;
 
 import io.cjf.jacartadministrationback.constant.ClientExceptionConstant;
 import io.cjf.jacartadministrationback.exception.ClientException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,6 +16,8 @@ import java.util.UUID;
 @RequestMapping("/image")
 @CrossOrigin
 public class ImageController {
+    @Value("${www.image.baseurl}")
+    private String imageBaseurl;
 
     private List<String> imageExts= Arrays.asList("jpg","jpeg","png");
 
@@ -36,6 +39,6 @@ public class ImageController {
             byte[] data = image.getBytes();
             out.write(data);
         }
-        return filename;
+        return imageBaseurl + "/" + filename;
     }
 }
