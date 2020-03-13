40ebd09c44e9f5b785bbb1f87dd9d57eda0b7027
WCY <13216142707>
2020-03-13 21:02:31 +0800

    修改后的

--- 电商前端/viewmodels/menu-navigate.js
+++ 电商前端/viewmodels/menu-navigate.js
@@ -1,47 +1,43 @@
 var app = new Vue({
+    router: router,
     el: '#app',
     data: {
-        selectMainPage: '',
         subMenus: [
             {
-                name: "商品管理",
-                index: "1",
-                icon: "el-icon-goods",
+                name: '商品管理',
+                index: '1',
+                icon: 'el-icon-goods',
                 menuItems: [
-                    { name: "商品列表", index: "1-1" }
+                    { name: '商品列表', index: '1-1', route: '/product/search' }
                 ]
             },
             {
-                name: "客户管理",
-                index: "2",
-                icon: "el-icon-s-custom",
+                name: '客户管理',
+                index: '2',
+                icon: 'el-icon-s-custom',
                 menuItems: [
-                    { name: "客户列表", index: "2-2" }
+                    { name: '客户列表', index: '2-2', route: '/customer/search' }
                 ]
             },
             {
-                name: "订单管理",
-                index: "3",
-                icon: "el-icon-s-order",
+                name: '订单管理',
+                index: '3',
+                icon: 'el-icon-s-order',
                 menuItems: [
-                    { name: "订单列表", index: "3-3" },
-                    { name: "退货列表", index: "3-4" }
+                    { name: '订单列表', index: '3-3', route: '/order/search' },
+                    { name: '退货列表', index: '3-4', route: '/return/search' }
                 ]
             },
             {
-                name: "用户管理",
-                index: "4",
-                icon: "el-icon-user",
+                name: '用户管理',
+                index: '4',
+                icon: 'el-icon-user',
                 menuItems: [
-                    { name: "用户列表", index: "4-5" }
+                    { name: '用户列表', index: '4-5', route: '/administrator/index' }
                 ]
             }
         ]
     },
     methods: {
-        handleMenuItemSelect(index, indexPath) {
-            console.log('menu item selected', index, indexPath);
-            this.selectMainPage = index;
-        }
     }
 })
